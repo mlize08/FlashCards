1cf526447133e3e2a7f24b6dc2bf3b1b755e8b98
Monalize M <150396638+mlize08@users.noreply.github.com>
2024-11-14 14:41:49 -0300

    S

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ function criaCartao(categoria, pergunta, resposta){
     function viraCartao(){
         respostaestaVisivel = !respostaEstaVisivel
         cartao.classList.toggle('active', respostaEstaVisivel)
-        cartao.addEventListener('click', viraCartao)
     }
-       container.appendChild(cartao)
+        cartao.addEventListener('click', viraCartao)
+        container.appendChild(cartao)
 
 }
